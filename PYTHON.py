5522ea8fd4fb14314bdef865209616afc93d46fb
Uma Pathapalyam <150056525+Uma213@users.noreply.github.com>
2023-12-03 02:19:57 +0530

    Add files via upload

---
+++ PYTHON.py
@@ -0,0 +1,25 @@
+str1= input("str: ")
+
+# Convert the string to uppercase print(strl.upper())
+
+# Convert the string to title case print(strl.title()) # Split the string into a list of words
+
+print(strl.split())
+
+width= int (input("width: "))
+
+fill_char= input("fillchar: ")
+
+# Center align the string with given width and fill character print(strl.center(width, fill_char))
+
+# Convert the string to lowercase print(strl.lower())
+
+str2 = input("Enter a joining character: ")
+
+# Join the characters of the string with the given character print (str2.join(strl))
+
+replace_old = input("old substring: ")
+
+replace_new = input("new substring: ")
+
+# Replace occurrences of old substring with new substring print(strl.replace(replace_old,replace_new))
